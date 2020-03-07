cada884f5c3d653185f941352e08e528b41b9194
YinChang <yincahng0626@gmail.com>
2020-03-07 13:54:36 +0800

    add angular by nx

---
+++ src/lib/@theme/theme.module.ts
@@ -0,0 +1,90 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  NbActionsModule,
+  NbLayoutModule,
+  NbMenuModule,
+  NbSearchModule,
+  NbSidebarModule,
+  NbUserModule,
+  NbContextMenuModule,
+  NbButtonModule,
+  NbSelectModule,
+  NbIconModule,
+  NbThemeModule,
+} from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { NbSecurityModule } from '@nebular/security';
+
+
+import {FooterComponent} from './components/footer/footer.component';
+import {HeaderComponent} from './components/header/header.component';
+import {SearchInputComponent} from './components/search-input/search-input.component';
+import {TinyMCEComponent} from './components/tiny-mce/tiny-mce.component';
+
+import {CapitalizePipe} from './pipes/capitalize.pipe';
+import {PluralPipe} from './pipes/plural.pipe';
+import {RoundPipe} from './pipes/round.pipe';
+import {TimingPipe} from './pipes/timing.pipe';
+import {NumberWithCommasPipe} from './pipes/number-with-commas.pipe';
+
+import {OneColumnLayoutComponent} from './layouts/one-column/one-column.layout';
+import {ThreeColumnsLayoutComponent} from './layouts/three-columns/three-columns.layout';
+import {TwoColumnsLayoutComponent} from './layouts/two-columns/two-columns.layout';
+
+import { DEFAULT_THEME } from './styles/theme.default';
+import { COSMIC_THEME } from './styles/theme.cosmic';
+import { CORPORATE_THEME } from './styles/theme.corporate';
+import { DARK_THEME } from './styles/theme.dark';
+
+const NB_MODULES = [
+  NbLayoutModule,
+  NbMenuModule,
+  NbUserModule,
+  NbActionsModule,
+  NbSearchModule,
+  NbSidebarModule,
+  NbContextMenuModule,
+  NbSecurityModule,
+  NbButtonModule,
+  NbSelectModule,
+  NbIconModule,
+  NbEvaIconsModule,
+];
+const COMPONENTS = [
+  HeaderComponent,
+  FooterComponent,
+  SearchInputComponent,
+  TinyMCEComponent,
+  OneColumnLayoutComponent,
+  ThreeColumnsLayoutComponent,
+  TwoColumnsLayoutComponent,
+];
+const PIPES = [
+  CapitalizePipe,
+  PluralPipe,
+  RoundPipe,
+  TimingPipe,
+  NumberWithCommasPipe,
+];
+
+@NgModule({
+  imports: [CommonModule, ...NB_MODULES],
+  exports: [CommonModule, ...PIPES, ...COMPONENTS],
+  declarations: [...COMPONENTS, ...PIPES],
+})
+export class ThemeModule {
+  static forRoot(): ModuleWithProviders {
+    return <ModuleWithProviders>{
+      ngModule: ThemeModule,
+      providers: [
+        ...NbThemeModule.forRoot(
+          {
+            name: 'default',
+          },
+          [ DEFAULT_THEME, COSMIC_THEME, CORPORATE_THEME, DARK_THEME ],
+        ).providers,
+      ],
+    };
+  }
+}
