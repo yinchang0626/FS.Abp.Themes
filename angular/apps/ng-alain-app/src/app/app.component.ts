ba5eb0cfce830fb23a7aeed13ce7b2782de6b320
ChangYinShung <changyenshung@gmail.com>
2020-03-10 14:14:31 +0800

    feat: add example set title and meta ,change default value

--- angular/apps/ng-alain-app/src/app/app.component.ts
+++ angular/apps/ng-alain-app/src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { VERSION as VERSION_ALAIN, TitleService } from '@delon/theme';
 import { VERSION as VERSION_ZORRO, NzModalService } from 'ng-zorro-antd';
+import { Store } from '@ngxs/store';
+import { ConfigState } from '@abp/ng.core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-root',
@@ -16,16 +19,30 @@ export class AppComponent implements OnInit {
         renderer: Renderer2,
         private router: Router,
         private titleSrv: TitleService,
+        private htmlSrv: Title ,
         private modalSrv: NzModalService,
+        private store:Store,
+        private meta: Meta
     ) {
         renderer.setAttribute(el.nativeElement, 'ng-alain-version', VERSION_ALAIN.full);
         renderer.setAttribute(el.nativeElement, 'ng-zorro-version', VERSION_ZORRO.full);
+        this.loadAbpSetting();
     }
 
     ngOnInit() {
         this.router.events.pipe(filter(evt => evt instanceof NavigationEnd)).subscribe(() => {
-            this.titleSrv.setTitle();
             this.modalSrv.closeAll();
+
         });
+
+        
+    }
+    
+    loadAbpSetting(){
+        let settingTitle = this.store.selectSnapshot(ConfigState.getSetting('Themes.Website.Title'));
+        let description =  this.store.selectSnapshot(ConfigState.getSetting('Themes.Website.Description'));
+        this.titleSrv.prefix = settingTitle;
+        this.htmlSrv.setTitle(settingTitle);
+        this.meta.updateTag({name:'description',content:description});
     }
 }
--- src/FS.Abp.Themes.Domain/Settings/ThemesSettingDefinitionProvider.cs
+++ src/FS.Abp.Themes.Domain/Settings/ThemesSettingDefinitionProvider.cs
@@ -14,31 +14,31 @@ namespace FS.Abp.Themes.Settings
             context.Add(
                     new SettingDefinition(
                         ThemesSettings.Website.Title,
-                        "ThemesSettings.Website.Title",
+                        "Themes.Website.Title",
                         L("DisplayName:FS.Themes.Website.Title_DsiplayName"),
                         L("Description:FS.Themes.Website.Title_Description"),
                         isVisibleToClients: true),
 
                     new SettingDefinition(ThemesSettings.Website.Description,
-                        "ThemesSettings.Website.Description",
+                        "Themes.Website.Description",
                         L("DisplayName:FS.Themes.Website.Description_DsiplayName"),
                         L("Description:FS.Themes.Website.Description_Description"),
                         isVisibleToClients: true),
 
                     new SettingDefinition(ThemesSettings.LoginPage.Title,
-                        "ThemesSettings.LoginPage.Title",
+                        "Themes.LoginPage.Title",
                         L("DisplayName:FS.Themes.LoginPage.Title_DsiplayName"),
                         L("Description:FS.Themes.LoginPage.Title_Description")
                         , isVisibleToClients: true),
                     new SettingDefinition(ThemesSettings.LoginPage.Description,
-                        "ThemesSettings.Website.Description",
+                        "Themes.Website.Description",
                         L("DisplayName:FS.Themes.LoginPage.Description_DsiplayName"),
                         L("Description:FS.Themes.LoginPage.Description_Description"),
                         isVisibleToClients: true
                         ),
 
                     new SettingDefinition(ThemesSettings.LoginPage.Footer,
-                        "ThemesSettings.Website.Footer",
+                        "Themes.Website.Footer",
                         L("DisplayName:FS.Themes.LoginPage.Footer_DsiplayName"),
                         L("Description:FS.Themes.LoginPage.Footer_Description"),
                         isVisibleToClients: true)
