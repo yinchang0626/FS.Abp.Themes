2a9243470dddcadeb8401ccb305fde48c799eb58
YinChang <yincahng0626@gmail.com>
2020-03-28 10:39:23 +0800

    move themes/core to libs/core

--- angular/libs/core/src/lib/core.module.ts
+++ angular/libs/core/src/lib/core.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
+import { CoreModule as AbpCoreModule } from '@abp/ng.core';
 
 export const themesRoutes: Route[] = [];
 
 @NgModule({
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, AbpCoreModule, RouterModule]
 })
-export class ThemesModule {}
+export class CoreModule { }
