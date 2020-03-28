ef2d48ebe364a81b2757f020fe346a08556c9b93
YinChang <yincahng0626@gmail.com>
2020-03-28 10:41:55 +0800

    subtree for libs/core

--- angular/libs/core/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'themes',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/themes',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
