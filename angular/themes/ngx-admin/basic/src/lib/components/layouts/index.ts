4846556bc1339a660c8658b32120494f19a7f1a7
YinChang <yincahng0626@gmail.com>
2020-04-01 15:42:49 +0800

    rejoin ngx-admin

--- angular/themes/ngx-admin/basic/src/lib/components/layouts/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './one-column/one-column.layout';
--- angular/themes/ngx-admin/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'ngx-admin',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/themes/ngx-admin',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
