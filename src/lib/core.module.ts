fd7c013686fe0ee7b4c742fa9caf35c2e70e586c
YinChang <yincahng0626@gmail.com>
2020-04-06 16:00:04 +0800

    add development in fs.core

--- src/lib/core.module.ts
+++ src/lib/core.module.ts
@@ -1,11 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders, InjectionToken, APP_INITIALIZER } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
-import { CoreModule as AbpCoreModule } from '@abp/ng.core';
+import { CoreModule as AbpCoreModule,noop } from '@abp/ng.core';
 
 export const themesRoutes: Route[] = [];
 import { NgxsModule } from '@ngxs/store';
 import { RouterState } from './states/router.state';
+import { CoreOptionsService } from './services/core-options.service';
+
+export interface CoreOptions {
+  showDev: boolean;
+}
+
+export function coreOptionsFactory(options: CoreOptions) {
+  return {
+    showDev: true,
+    ...options,
+  };
+}
+
+export const CoreOptions = new InjectionToken('CoreOptions');
+
 @NgModule({
   imports: [
     CommonModule, 
@@ -15,4 +30,19 @@ import { RouterState } from './states/router.state';
     NgxsModule.forFeature([ RouterState ])
   ]
 })
-export class CoreModule { }
+export class CoreModule { 
+  static forRoot(options = {} as CoreOptions): ModuleWithProviders {
+    return {
+      ngModule: CoreModule,
+      providers: [
+        { provide: CoreOptions, useValue: options },
+        {
+          provide: 'CoreOptions',
+          useFactory: coreOptionsFactory,
+          deps: [CoreOptions],
+        },
+        { provide: APP_INITIALIZER, deps: [CoreOptionsService], useFactory: noop, multi: true }
+      ],
+    };
+  }  
+}
---
+++ src/lib/services/core-options.service.ts
@@ -0,0 +1,30 @@
+import { AddRoute, ABP, addAbpRoutes, eLayoutType, GetAppConfiguration, PatchRouteByName, ConfigStateService, ConfigState } from '@abp/ng.core';
+import { Injectable, Inject, Optional } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store, Actions, ofActionDispatched } from '@ngxs/store';
+import { CodesTreeService } from '@fs/coding-management/core';
+import { timeInterval, tap, map, first } from 'rxjs/operators';
+
+//todo 改rxjs對應取得config後的事件
+@Injectable({
+  providedIn: 'root'
+})
+export class CoreOptionsService {
+  constructor(
+    @Optional() @Inject('OTA_OPTIONS') private options: any
+  ) {
+    let showDev = !options || options.showDev;
+
+    addAbpRoutes([
+      {
+        name: 'Development',
+        path: '',
+        order: 0,
+        wrapper: true,
+        iconClass: 'fa fa-wrench',
+        invisible: !showDev,
+        children:[]
+      }
+    ]);
+  }
+}
